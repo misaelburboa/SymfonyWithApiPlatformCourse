3bd897c8f898c91d2be9dc30bc622407e90f7300
Misael Burboa <mburboa@pipingrock.com>
2019-07-25 22:08:15 -0700

    Grant operations acces by roles

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -26,13 +26,13 @@ use ApiPlatform\Core\Annotation\ApiSubresource;
  *             }
  *          },
  *          "put"={
- *              "access_control"="is_granted('IS_AUTHENTICATED_FULLY') and object.getAuthor() == user"
+ *              "access_control"="is_granted('ROLE_EDITOR') or (is_granted('ROLE_WRITER') and object.getAuthor() == user)"
  *          }
  *      },
  *      collectionOperations={
  *          "get",
  *          "post"={
- *              "access_control"="is_granted('IS_AUTHENTICATED_FULLY')"
+ *              "access_control"="is_granted('ROLE_WRITER')"
  *          }
  *      }
  * )
--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -20,13 +20,13 @@ use Symfony\Component\Serializer\Annotation\Groups;
  *      itemOperations={
  *          "get",
  *          "put"={
- *              "access_control"="is_granted('IS_AUTHENTICATED_FULLY') and object.getAuthor() == user"
+ *              "access_control"="is_granted('ROLE_EDITOR') or (is_granted('ROLE_COMMENTATOR') and object.getAuthor() == user)"
  *          }
  *      },
  *      collectionOperations={
  *          "get",
  *          "post"={
- *              "access_control"="is_granted('IS_AUTHENTICATED_FULLY')"
+ *              "access_control"="is_granted('ROLE_COMMENTATOR')"
  *          }
  *      },
  *      subresourceOperations={
